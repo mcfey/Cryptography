de57cf965537bfbd125a9e56469936b3daa8da59
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-02-29 14:25:13 -0500

    Updated from Brython Server: 2/29/2016 2:26:20 PM

--- cryptography.py
+++ cryptography.py
@@ -32,7 +32,7 @@ while command!="q":
         
         n = len(mlist)
         while n>0:
-            klist.append(k(n-(n-1)))
+            klist.append(klist(n-(n-1)))
             n=n-1
         print(klist)
     
